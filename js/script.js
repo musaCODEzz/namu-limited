1d60c9a148521cd58827e7ef2aedfac09a770964
Max <57855468+musaCODEzz@users.noreply.github.com>
2023-02-07 16:25:57 +0300

    Login form

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var menu = document.querySelector('#menu-btn');
+var navbar = document.querySelector('.navbar');
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+   
+}
+
+window.onscroll = () => {
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () => {
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+window.onload = () => {
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+
+
+    
+}
