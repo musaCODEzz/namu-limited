e32900022e66be1ef5d8f789e14ee040fd2d9a37
Max <57855468+musaCODEzz@users.noreply.github.com>
2023-02-22 15:42:42 +0300

    Code separation

--- js/signin.js
+++ js/signin.js
@@ -1,19 +1,8 @@
 
-const firebaseApp = firebase.initializeApp({ 
-    apiKey: "AIzaSyA2cpiKOc-QTntZhixngoDJPQNeBBr0L20",
-    authDomain: "trial-66a86.firebaseapp.com",
-    databaseURL: "https://trial-66a86-default-rtdb.firebaseio.com",
-    projectId: "trial-66a86",
-    storageBucket: "trial-66a86.appspot.com",
-    messagingSenderId: "656510144104",
-    appId: "1:656510144104:web:4d7e25348fe003a91df5dd"
+// importing firebase config
+import { firebaseApp } from './config.js';
 
-
-
- });
-
- const db = firebaseApp.firestore();
- const auth = firebaseApp.auth();
+const auth = firebaseApp.auth();
 const realtimedb = firebaseApp.database();
 
 
--- js/signup.js
+++ js/signup.js
@@ -1,26 +1,17 @@
-const firebaseApp = firebase.initializeApp({ 
-    apiKey: "AIzaSyA2cpiKOc-QTntZhixngoDJPQNeBBr0L20",
-    authDomain: "trial-66a86.firebaseapp.com",
-    projectId: "trial-66a86",
-    storageBucket: "trial-66a86.appspot.com",
-    messagingSenderId: "656510144104",
-    appId: "1:656510144104:web:4d7e25348fe003a91df5dd"
+// importing firebase config
+import { firebaseApp } from './config.js';
 
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const today = new Date();
 
+document.getElementById("signup").addEventListener('click', function(event){
+   event.preventDefault();
 
- });
-
- const db = firebaseApp.firestore();
- const auth = firebaseApp.auth();
- const today = new Date();
-
- document.getElementById("signup").addEventListener('click', function(event){
-    event.preventDefault();
-
-    const email = document.getElementById('1email').value;
-    const password = document.getElementById('1password').value;
-    console.log(email, password);
-    auth.createUserWithEmailAndPassword(email, password)
+   const email = document.getElementById('1email').value;
+   const password = document.getElementById('1password').value;
+   console.log(email, password);
+   auth.createUserWithEmailAndPassword(email, password)
     .then((userCredentials) => {
     console.log(userCredentials.user);
       db.collection('users').doc(userCredentials.user.uid).set({
@@ -28,12 +19,12 @@ const firebaseApp = firebase.initializeApp({
          password: password,
          date: today
       })
+      window.location.href = "/namu-limited/html/index.html";
 
    })
    .catch((err) => {
     console.log(err.code);
     alert(err.message);
    }) 
-
 });
 
